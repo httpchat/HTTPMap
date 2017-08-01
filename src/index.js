6aaf44a8c74b6eab9e1f0cbaf0e2b6ed76fd7a42
Ben Schroeder <bschroeder@esri.com>
2017-08-01 13:54:44 -0400

    More webpack configurations

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-// Import dependencies
+// Set up Leaflet
 import L from 'leaflet';
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
+// Import other dependencies
+import axios from 'axios';
 
 // Import CSS
 import 'leaflet/dist/leaflet.css';
@@ -14,3 +23,11 @@ const myBasemap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
   attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 });
 myBasemap.addTo(httpMap);
+
+
+axios.get('httpchat-members.json')
+  .then(res => {
+    res.data.map(member => {
+      L.marker([member.latitude, member.longitude]).bindPopup(member.user).addTo(httpMap);
+    })
+  });
--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,8 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  devServer: {
-    contentBase: './dist'
-  },
   plugins: [
-    new CleanWebpackPlugin(['dist'])
+    new CleanWebpackPlugin(['./dist'])
   ],
   output: {
     filename: 'bundle.js',
@@ -30,7 +27,7 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
-          'file-loader'
+          'file-loader',
         ]
       }
     ]
